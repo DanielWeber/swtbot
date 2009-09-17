2e2dcefa6944773a9317c568a8bfe4ba67538a8d
DanielWeber <daniel.weber.dev@googlemail.com>
2009-09-17 10:01:40 +0200

    Made the tree DND test more reliable and readable...

--- org.eclipse.swtbot.swt.finder.test/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotDragAndDropTest.java
+++ org.eclipse.swtbot.swt.finder.test/src/org/eclipse/swtbot/swt/finder/widgets/SWTBotDragAndDropTest.java
@@ -12,11 +12,9 @@ package org.eclipse.swtbot.swt.finder.widgets;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.eclipse.swtbot.swt.finder.finders.AbstractSWTTestCase;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -39,22 +37,14 @@ public class SWTBotDragAndDropTest extends AbstractSWTTestCase {
 		bot.comboBoxInGroup("Widget", 0).setSelection("Tree");
 		bot.comboBoxInGroup("Widget", 1).setSelection("Tree");
 		final SWTBotTree sourceTree = bot.tree(0);
-		final SWTBotTreeItem sourceItem = sourceTree.getTreeItem("Drag Source name 0");
 		final SWTBotTree targetTree = bot.tree(1);
+		final SWTBotTreeItem sourceItem = sourceTree.getTreeItem("Drag Source name 0");
+		final SWTBotTreeItem targetItem = targetTree.getTreeItem("Drop Target name 0");
 
-		// dragged element does not exist in the target before DND operation:
-		for (final SWTBotTreeItem item : targetTree.getAllItems()) {
-			assertFalse("Widget 'Drag Source name 0' should not have been found", item.getText().equals("Drag Source name 0"));
-		}
-
-		sourceItem.dragAndDrop(targetTree);
-
-		// dragged element does no longer exist in the source tree...
-		for (final SWTBotTreeItem item : sourceTree.getAllItems()) {
-			assertFalse("Widget 'Drag Source name 0' should not have been found", item.getText().equals("Drag Source name 0"));
-		}
-		// ...but in the target
-		targetTree.getTreeItem("Drag Source name 0");
+		assertDoesNotContain(targetTree, "Drag Source name 0");
+		sourceItem.dragAndDrop(targetItem);
+		assertDoesNotContain(sourceTree, "Drag Source name 0");
+		assertTrue(targetItem.getNodes().contains("Drag Source name 0"));
 	}
 
 	@Test
@@ -71,5 +61,19 @@ public class SWTBotDragAndDropTest extends AbstractSWTTestCase {
 		// This is odd. At least under windows, drag'n'dropping text appends a newline to the end of the text box' text.
 		// Is that done deliberately? Or by the SWTExample implementation?
 		assertEquals("Drop Target TextDrag Source Text\n", targetText.getText());
+	}
+
+	private void assertDoesNotContain(SWTBotTree targetTree, String itemText) {
+		for (final SWTBotTreeItem item : targetTree.getAllItems()) {
+			assertFalse("Widget '" + itemText + "' should not have been found", item.getText().equals(itemText));
+			assertDoesNotContain(item, itemText);
+		}
+	}
+
+	private void assertDoesNotContain(SWTBotTreeItem rootItem, String itemText) {
+		for (final SWTBotTreeItem item : rootItem.getItems()) {
+			assertFalse("Widget '" + itemText + "' should not have been found", item.getText().equals(itemText));
+			assertDoesNotContain(item, itemText);
+		}
 	}
 }
